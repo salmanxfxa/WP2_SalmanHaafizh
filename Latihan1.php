f50892ebc49809ae0465ad7aefa48db1c97f3cc6
salmanxfxa <salmanxfxa@gmail.com>
2021-11-08 21:35:07 +0700

    Menambahkan file Latihan1.php

---
+++ Latihan1.php
@@ -0,0 +1,23 @@
+<?php 
+
+Class Latihan1 extends CI_Controller
+{
+
+	public function index()
+	{
+		echo "Selamat datang, selamat belajar web programing";
+		//$this->load->view('view-latihan1');
+	}
+
+	public function penjumlahan($n1, $n2)
+ 	{
+ 		$this->load->model('Model_latihan1');
+ 
+ 		$data['nilai1'] = $n1;
+ 		$data['nilai2'] = $n2;
+ 		$data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+
+ 		$this->load->view('view-latihan1', $data);
+ 	}
+
+}
