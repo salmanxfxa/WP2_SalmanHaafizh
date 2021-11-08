c1d1e1ba6522c04789af73db65256e704eb916af
salmanxfxa <salmanxfxa@gmail.com>
2021-11-08 20:28:03 +0700

    Menambahkan file Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+Class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Salman Haafizh
+			 Saya tinggal di daerah Cikarang
+			 Saya gemar bermain game";
+	}
+}
