9ac600906fce140bf169a99973b772a6178ae5d8
salmanxfxa <90628956+salmanxfxa@users.noreply.github.com>
2021-11-08 22:52:03 +0700

    Menambah file welcome-message pada folder view

---
+++ welcome_message.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Latihan 1</title>
+
+</head>
+
+<body>
+
+	Halo kawan.. Yuk kita belajar web programing ..!!<br>
+	Nilai 1 = <?php echo $nilai1 ?>
+	Nilai 2 = <?= $nilai2 ?>
+
+	Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+
+</body>
+
+</html>
